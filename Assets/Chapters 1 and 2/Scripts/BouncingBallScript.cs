ff197f890175a5ec9f47c2284b0ae7ba1b715a93
Dion Rene Xavier Upton-Forrest <tazermedown@gmail.com>
2015-07-02 07:02:36 +0100

    ok

---
+++ Assets/Chapters 1 and 2/Scripts/BouncingBallScript.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BouncingBallScript : MonoBehaviour 
+{
+	public int jumpSpeed = 10;
+	public bool canJump = false;
+	public string myString = "Dion lol";
+
+	void Start ()
+	{
+
+	}
+	void Update () 
+	{
+		if (Input.GetButtonDown ("Jump") && canJump == true)
+			GetComponent<Rigidbody> ().AddForce (0, jumpSpeed, 0);
+
+		if (Input.GetButtonDown ("Fire1")) {
+			jumpSpeed += 100;
+			Debug.Log ("Your jump speed is now: " + jumpSpeed);
+		}
+
+		if (Input.GetButtonDown ("Fire2")) {
+			canJump = true;
+			Debug.Log ("Can jump is true");
+		}
+	}
+}
---
+++ Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerScript : MonoBehaviour {
+
+	public float moveSpeed = 5;
+	public float rotateSpeed = 180;
+	public float jumpSpeed = 20;
+	public float gravity = 9.8f;
+
+	Vector3 currentMovement;
+	void Start () {
+	
+	}
+	
+	void Update () {
+		CharacterController controller = GetComponent<CharacterController>();
+
+		transform.Rotate (0,Input.GetAxis ("Horizontal") * rotateSpeed * Time.deltaTime ,0);
+
+
+		currentMovement = new Vector3(0, currentMovement.y ,Input.GetAxis("Vertical") * moveSpeed) ;
+
+		currentMovement = transform.rotation * currentMovement;
+
+		if (!controller.isGrounded)
+			currentMovement -= new Vector3 (0, gravity * Time.deltaTime, 0);
+		else
+			currentMovement.y = 0;
+			
+
+		if (controller.isGrounded && Input.GetButtonDown ("Jump"))
+			currentMovement.y = jumpSpeed;
+
+
+		controller.Move (currentMovement * Time.deltaTime);
+	}
+}
